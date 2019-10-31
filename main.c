e9755a41543d7cca55f97df169ebede27cceed8b
Ronic <abdoronic@gmail.com>
2019-10-31 20:20:59 +0200

    Added random sample generator

--- main.c
+++ main.c
@@ -1,6 +1,46 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * sample -> contains the values of the sample.
+ * sampleSize -> the number of elements in the sample.
+ */
+
+int *sample, sampleSize;
+
+/*
+ * @param size the size of the sample.
+ * @param maxElement the maximum element in the sample.
+ * Generates the sample to apply the calculations on.
+ */
+
+void generateRandomSample(int size, int maxElement) {
+    srand((unsigned)time(0));
+    sampleSize = size;
+    sample = malloc(sampleSize * sizeof(int));
+    for(int i = 0; i < sampleSize; i++)
+        sample[i] = rand() % (maxElement + 1);
+}
+
+void printSample() {
+    printf("{");
+    for(int i = 0; i < sampleSize; i++)
+        printf("%d%s", sample[i], i < sampleSize - 1 ? ", " : "}\n");
+}
+
+int summation();
+
+double mean();
+
+double variance();
+
+double standardDeviation();
+
+int mode();
 
 int main() {
-    printf("Hello, World!\n");
+    generateRandomSample(10, 100);
+    printSample();
     return 0;
 }
