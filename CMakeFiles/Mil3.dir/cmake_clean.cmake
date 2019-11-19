2bc9fecc4f1914f6d3dbf842a01547e49c95be33
Ouda <abde.hassan2001@gmail.com>
2019-11-19 14:33:58 +0200

    Mil3

---
+++ CMakeFiles/Mil3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Mil3.dir/main.c.o"
+  "Mil3.pdb"
+  "Mil3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Mil3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
